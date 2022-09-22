7d94d2375eef8346ac2288f8aa1c2e2192d44870
shovit84 <sjshovit.jk@gmail.com>
2022-09-22 13:32:29 +0545

    third commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,29 +8,31 @@
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Font Awesome CDN Link for Icons -->
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="http://www.unicodenepali.com/v3/j.js"></script>
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="{{asset('js/unicode.js')}}"></script>
 </head>
 
 <body>
-    <div class="container">
-        <div class="wrapper">
-            <div class="text-input">
-                <textarea spellcheck="false" name="textarea1" class="UnicodeNepali" id="input" class="from-text" placeholder="Enter text"></textarea>
+    <form>
+
+        <div class="container">
+            <div class="wrapper">
+                <div class="text-input">
+                    <textarea spellcheck="false" name="unicodenepali1" class="UnicodeNepali" id="input" class="from-text"
+                        placeholder="Enter text"></textarea>
+                </div>
+
             </div>
-
+            <button id="copy">Copy</button>
+            <button id="clear">Clear</button>
         </div>
-        <button id="copy">Copy</button>
-        <button id="clear">Clear</button>
-
-
-    </div>
-
+    </form>
     <script>
         $(document).ready(function() {
-            InitializeUnicodeNepali();
+
             // Copy
 
             $("#copy").click(function() {
@@ -43,12 +45,10 @@
                 $('#input').val("");
 
             });
+            InitializeUnicodeNepali();
         });
     </script>
 
-    <script language="javascript" type="text/javascript">
-
-    </script>
 </body>
 
 </html>
