c4e540153bb9bf41b9d40597bc038b4e334d5fc8
shovit84 <sjshovit.jk@gmail.com>
2022-09-22 12:33:17 +0545

    second commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,14 +9,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Font Awesome CDN Link for Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://www.unicodenepali.com/v3/j.js"></script>
 </head>
 
 <body>
     <div class="container">
         <div class="wrapper">
             <div class="text-input">
-                <textarea spellcheck="false" id="input" class="from-text" placeholder="Enter text"></textarea>
-                <textarea spellcheck="false" readonly class="to-text" id="output" placeholder="Translation"></textarea>
+                <textarea spellcheck="false" name="textarea1" class="UnicodeNepali" id="input" class="from-text" placeholder="Enter text"></textarea>
             </div>
 
         </div>
@@ -25,28 +27,27 @@
 
 
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
     <script>
         $(document).ready(function() {
-            $("#input").change(function() {
-                let text = $("#input").val();
-                $("#output").val(text);
-            });
+            InitializeUnicodeNepali();
             // Copy
 
             $("#copy").click(function() {
-                var mytxtcontent = $("#output").val();
+                var mytxtcontent = $("#input").val();
                 navigator.clipboard.writeText(mytxtcontent);
 
             });
             //Reset
-            $('#clear').click(function(){
+            $('#clear').click(function() {
                 $('#input').val("");
-                $('#output').val("");
 
             });
         });
+    </script>
+
+    <script language="javascript" type="text/javascript">
+
     </script>
 </body>
 
